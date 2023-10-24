3cf663d6e1c569b1391399f40c36e1f985e701fd
Jean Augusto <jzinformatica@gmail.com>
2023-10-24 17:43:55 -0300

    refactor: clear database between tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -8,6 +8,11 @@ describe('Account Mongo Repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany()
+  })
   const makesut = (): AccountMongoRepository => {
     return new AccountMongoRepository()
   }
