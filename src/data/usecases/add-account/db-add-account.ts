346b88716283e45924449a8bd487296807ebc7af
Jean Augusto <jzinformatica@gmail.com>
2023-10-24 13:29:43 -0300

    refactor: remove console.log

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -17,8 +17,6 @@ export class DbAddAccount implements AddAccount {
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
-
-    console.log('hashedPassword', hashedPassword)
     const account = await this.addAccountRepository.add(
       Object.assign({}, accountData, { password: hashedPassword }),
     )
