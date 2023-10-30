785bc1a7407e786ce0f0fe4a0bf9b08aeab849b7
Jean Augusto <jzinformatica@gmail.com>
2023-10-30 10:00:44 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helper/validators/compare-fields-validation.spec.ts
+++ src/presentation/helper/validators/compare-fields-validation.spec.ts
@@ -1,14 +1,17 @@
 import { RequiredFieldValidation } from './required-field-validation'
 import { MissingParamError } from '../../errors'
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
 describe('Required Field Validattion', () => {
   test('Should return a MissinParamError if validation fails', async () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ name: 'any_field' })
     expect(error).toEqual(new MissingParamError('field'))
   })
   test('Should not return if validation succeeds', async () => {
-    const sut = new RequiredFieldValidation('field')
+    const sut = makeSut()
     const error = sut.validate({ field: 'any_field' })
     expect(error).toBeFalsy()
   })
