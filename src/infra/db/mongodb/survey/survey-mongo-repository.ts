3e690c389cb2d5dda0cc591f37f4cdfddd047184
Jean Augusto <jzinformatica@gmail.com>
2023-11-14 10:07:20 -0300

    docs: update requirements

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -5,6 +5,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 export class SurveyMongoRepository implements AddSurveyRepository {
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
+    console.log(surveyCollection, surveyData)
     await surveyCollection.insertOne(surveyData)
   }
 }
