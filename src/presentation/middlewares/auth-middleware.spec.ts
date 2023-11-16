10973bbacd929d0752ec35de05ce07615ddee18a
Jean Augusto <jzinformatica@gmail.com>
2023-11-16 13:28:24 -0300

    test: ensure AuthMiddleaware retuns 403 if LoadAccountByToken retunrs null

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -54,4 +54,11 @@ describe('Auth Middleware', () => {
     await sut.handle(makeFakeRequest())
     expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
+
+  test('Should returns 403 if LoadAccountByToken retunrs null ', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'load').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const httpResponse = await sut.handle({} as any)
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
 })
