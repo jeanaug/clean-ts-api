4db3e417ff4d8647331ffda2aa5b73ec8e47d0ee
Jean Augusto <jzinformatica@gmail.com>
2023-11-24 14:49:42 -0300

    refactor: organize SurveyMongoRepository tests ins describes

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -21,24 +21,27 @@ describe('Survey Mongo Repository', () => {
   const makesut = (): SurveyMongoRepository => {
     return new SurveyMongoRepository()
   }
-  test('Should add a survey on success ', async () => {
-    const sut = makesut()
-    await sut.add({
-      question: 'any_question',
-      answers: [
-        {
-          image: 'any_image',
-          answer: 'any_answer',
-        },
-        {
-          answer: 'other_answer',
-        },
-      ],
-      date: new Date(),
+
+  describe('Add()', () => {
+    test('Should add a survey on success ', async () => {
+      const sut = makesut()
+      await sut.add({
+        question: 'any_question',
+        answers: [
+          {
+            image: 'any_image',
+            answer: 'any_answer',
+          },
+          {
+            answer: 'other_answer',
+          },
+        ],
+        date: new Date(),
+      })
+
+      const survey = await surveyCollection.findOne({ question: 'any_question' })
+
+      expect(survey).toBeTruthy()
     })
-
-    const survey = await surveyCollection.findOne({ question: 'any_question' })
-
-    expect(survey).toBeTruthy()
   })
 })
