3aa36d72db8394814536b3f6e89b54cb4e583410
Jean Augusto <jzinformatica@gmail.com>
2023-11-24 16:29:18 -0300

    feat: ensure Survey Routes retuns 403 if no access token is provided

--- src/main/factories/controllers/survey/add-survey/add-survey-factory.spec.ts
+++ src/main/factories/controllers/survey/add-survey/add-survey-factory.spec.ts
@@ -1,7 +1,7 @@
-import { Validation } from '../../../../presentation/protocols'
-import { RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
+import { Validation } from '../../../../../presentation/protocols'
+import { RequiredFieldValidation, ValidationComposite } from '../../../../../validation/validators'
 import { makeAddSurveyValidation } from './add-survey-validation-factory'
-jest.mock('../../../../validation/validators/validation-composite')
+jest.mock('../../../../../validation/validators/validation-composite')
 
 describe('AddSurveyValidationFactory', () => {
   test('Should call ValidationComposite with all validations', async () => {
--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
-import { makeAddSurveyController } from '../factories/controllers/add-survey/add-survey-controller-factory'
+import { makeAddSurveyController } from '../factories/controllers/survey/add-survey/add-survey-controller-factory'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAuthMiddleare } from '../factories/middlewares/auth-middleware-factory'
 import { adaptMiddleware } from '../adapters/express-middleware-adapter'
+import { makeLoadSurveysController } from '../factories/controllers/survey/load-surveys/load-surveys-controller-factory'
 
 export default (router: Router): void => {
   const adminAuth = adaptMiddleware(makeAuthMiddleare('admin'))
+  const auth = adaptMiddleware(makeAuthMiddleare())
   router.post('/surveys', adminAuth, adaptRoute(makeAddSurveyController()))
+  router.get('/surveys', auth, adaptRoute(makeLoadSurveysController()))
 }
