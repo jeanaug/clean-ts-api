461adfd6d506e47b8190a33f1c1fa52600241560
Jean Augusto <jzinformatica@gmail.com>
2023-11-24 13:49:36 -0300

    test: ensure LoadSurveysController retunrs 204 if LoadSurveys returns empty

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,7 +1,7 @@
 import { LoadSurveys, SurveyModel } from './load-survey-controller-protocols'
 import { LoadSurveysController } from './load-surveys-controller'
 import MockDate from 'mockdate'
-import { ok, serverError } from '../../../helper/http/http-helper'
+import { noContent, ok, serverError } from '../../../helper/http/http-helper'
 const makeFakeSurveys = (): SurveyModel[] => {
   return [
     {
@@ -65,6 +65,14 @@ describe('LoadSurveys Controller', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(ok(makeFakeSurveys()))
+  })
+
+  test('Should return 204 if LoadSurveys returns empty', async () => {
+    const { sut, loadSurveysStub } = makeSut()
+    jest.spyOn(loadSurveysStub, 'load').mockReturnValueOnce(new Promise(resolve => resolve([])))
+
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(noContent())
   })
 
   test('Should retunr 500 if LoadSurveys throws', async () => {
