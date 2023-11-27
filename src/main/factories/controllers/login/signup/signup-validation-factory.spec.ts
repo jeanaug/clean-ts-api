1e583a99d126bdbef14cd2a505d4bbe7d1a5550c
Jean Augusto <jzinformatica@gmail.com>
2023-11-27 19:06:10 -0300

    refactor: change path of Jest mocks to use @

--- src/main/factories/controllers/login/signup/signup-validation-factory.spec.ts
+++ src/main/factories/controllers/login/signup/signup-validation-factory.spec.ts
@@ -3,7 +3,7 @@ import { RequiredFieldValidation, CompareFieldsValidation, EmailValidation, Vali
 import { EmailValidator } from '@/validation/validators/protocols/email-validator'
 import { makeSigUpValidation } from './signup-validation-factory'
 
-jest.mock('../../../../../validation/validators/validation-composite')
+jest.mock('@/validation/validators/validation-composite')
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
     isValid(email: string): boolean {
