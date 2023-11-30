6ff30c7b36e989e80f31d4313b0f33f1479251dd
Jean Augusto <jzinformatica@gmail.com>
2023-11-30 15:44:58 -0300

    feat: ensure SaveSurveyResult returns 203 if LoadSurveyById returns null

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,7 +1,8 @@
 import { Controller, HttpRequest, HttpResponse, SaveSurveyResult } from './save-survey-result-controller-protocols'
 import { LoadSurveyByIdRepository } from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
 import { LoadSurveyById } from '@/domain/usecases/survey/load-surveys-by-id'
-import { ok, serverError } from '@/presentation/helper/http/http-helper'
+import { InvalidParamError } from '@/presentation/errors'
+import { forbidden, ok, serverError } from '@/presentation/helper/http/http-helper'
 
 export class SaveSurveyResultController implements Controller {
   constructor(
@@ -11,6 +12,7 @@ export class SaveSurveyResultController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
+      if (!survey) return forbidden(new InvalidParamError('Survey not exists'))
       const surveyResult = await this.saveSurveyResult.save(httpRequest.body)
       return surveyResult && ok(surveyResult)
     } catch (error) {
