6b9fdb37f14dc5e14c4d403baace13c12a6e8c6a
Jean Augusto <jzinformatica@gmail.com>
2023-11-30 15:33:47 -0300

    featt: ensure SaveSurveyResult calls LoadSurveyById with correct values

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,10 +1,16 @@
 import { Controller, HttpRequest, HttpResponse, SaveSurveyResult } from './save-survey-result-controller-protocols'
+import { LoadSurveyByIdRepository } from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
+import { LoadSurveyById } from '@/domain/usecases/survey/load-surveys-by-id'
 import { ok, serverError } from '@/presentation/helper/http/http-helper'
 
 export class SaveSurveyResultController implements Controller {
-  constructor(private readonly saveSurveyResult: SaveSurveyResult) {}
+  constructor(
+    private readonly loadSurveyById: LoadSurveyById,
+    private readonly saveSurveyResult: SaveSurveyResult,
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const survey = await this.loadSurveyById.loadById(httpRequest.params.surveyId)
       const surveyResult = await this.saveSurveyResult.save(httpRequest.body)
       return surveyResult && ok(surveyResult)
     } catch (error) {
