83727df830bd0ae56370f9114dc23a7d24bf7603
Jean Augusto <jzinformatica@gmail.com>
2023-11-30 12:11:45 -0300

    feat: ensure SaveSurveyResult retunrs 200 on success

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -1,13 +1,12 @@
-import { serverError } from '@/presentation/helper/http/http-helper'
+import { ok, serverError } from '@/presentation/helper/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, SaveSurveyResult } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
   constructor(private readonly saveSurveyResult: SaveSurveyResult) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      await this.saveSurveyResult.save(httpRequest.body)
-
-      return new Promise(resolve => resolve(null))
+      const surveyResult = await this.saveSurveyResult.save(httpRequest.body)
+      return surveyResult && ok(surveyResult)
     } catch (error) {
       return serverError(error)
     }
