e909a6c963b82b7c288203c6838f9c8cee2f2ab1
Jean Augusto <jzinformatica@gmail.com>
2023-11-30 08:49:10 -0300

    feat: ensure SaveSurveyResult retunrs 500 if SaveSurveyResult throws

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -1,10 +1,15 @@
+import { serverError } from '@/presentation/helper/http/http-helper'
 import { Controller, HttpRequest, HttpResponse, SaveSurveyResult } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
   constructor(private readonly saveSurveyResult: SaveSurveyResult) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.saveSurveyResult.save(httpRequest.body)
+    try {
+      await this.saveSurveyResult.save(httpRequest.body)
 
-    return new Promise(resolve => resolve(null))
+      return new Promise(resolve => resolve(null))
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
