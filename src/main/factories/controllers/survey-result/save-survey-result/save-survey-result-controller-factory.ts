095cdec8e5e0b53af90d700266829a51bceafcb9
Jean Augusto <jzinformatica@gmail.com>
2024-01-12 15:54:21 -0300

    feat: ensure load survey route returns 403 if no accessToke is provided

--- src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory.ts
+++ src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory.ts
@@ -1,8 +1,8 @@
-import { makeDbSaveSurveyResult } from '../../usecases/survey-result/save-survey-result/db-save-survey-result-factorys'
+import { makeDbSaveSurveyResult } from '../../../usecases/survey-result/save-survey-result/db-save-survey-result-factorys'
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
 import { Controller } from '@/presentation/protocols'
 import { SaveSurveyResultController } from '@/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller'
-import { makeDbLoadSurveyById } from '../../usecases/survey/load-survey-by-id/db-load-survey-by-id-factory'
+import { makeDbLoadSurveyById } from '../../../usecases/survey/load-survey-by-id/db-load-survey-by-id-factory'
 
 export const makeSaveSurveyResultController = (): Controller => {
   const controller = new SaveSurveyResultController(makeDbLoadSurveyById(), makeDbSaveSurveyResult())
